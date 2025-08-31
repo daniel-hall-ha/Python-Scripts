0b5caa29e2edeebe4363de611cae84adece6a4cb
daniel hall <ha.danielhall@gmail.com>
2025-08-31 19:46:09 +0630

    Revert "Added new feature that produces a list of IP and their frequencies"
    This reverts commit bc20200c878e12fd16fc453195b28c216e11461b.

--- tests/access_log_analyzer_test.py
+++ tests/access_log_analyzer_test.py
@@ -1,5 +1,5 @@
 import unittest
-from scripts.access_log_analyzer import input_file, extract_data_from_file, total_number_of_requests, IP_frequencies
+from scripts.access_log_analyzer import input_file, extract_data_from_file
 from unittest.mock import patch
 
 class UnitTests(unittest.TestCase):
@@ -252,127 +252,15 @@ class UnitTests(unittest.TestCase):
         ]
         self.assertEqual(result, extracted_data_list)
 
+'''
     def test_total_number_of_requests(self):
-        data_list = [
-            {
-                "ip": "127.167.171.1",
-                "timestamp": "29/Aug/2025:00:37:02",
-                "method": "GET",
-                "path": "/dashboard",
-                "protocol": "HTTP/1.1",
-                "status": 404,
-                "size": 7317,
-                "referer": "-",
-                "user_agent": "curl/7.68.0"
-            },
-            {
-                "ip": "117.74.190.180",
-                "timestamp": "29/Aug/2025:00:37:02",
-                "method": "DELETE",
-                "path": "/products",
-                "protocol": "HTTP/1.1",
-                "status": 500,
-                "size": 2803,
-                "referer": "-",
-                "user_agent": "Wget/1.20.3 (linux-gnu)"
-            },
-            {
-                "ip": "207.238.22.214",
-                "timestamp": "29/Aug/2025:00:37:02",
-                "method": "POST",
-                "path": "/static/js/app.js",
-                "protocol": "HTTP/1.1",
-                "status": 404,
-                "size": 4086,
-                "referer": "-",
-                "user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) Safari/605.1.15"
-            },
-            {
-                "ip": "41.177.11.204",
-                "timestamp": "29/Aug/2025:00:37:02",
-                "method": "POST",
-                "path": "/dashboard",
-                "protocol": "HTTP/1.1",
-                "status": 503,
-                "size": 2195,
-                "referer": "-",
-                "user_agent": "Wget/1.20.3 (linux-gnu)"
-            },
-            {
-                "ip": "182.218.163.134",
-                "timestamp": "29/Aug/2025:00:37:02",
-                "method": "DELETE",
-                "path": "/dashboard",
-                "protocol": "HTTP/1.1",
-                "status": 502,
-                "size": 2418,
-                "referer": "-",
-                "user_agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64) Firefox/113.0"
-        }]
-        result = total_number_of_requests(data_list)
-        self.assertEqual(result, 5)
+        result = total_number_of_requests([{},{}])
+        self.assertEqual(result, "")
 
     def test_IP_frequencies(self):
-        data_list = [
-            {
-                "ip": "127.167.171.1",
-                "timestamp": "29/Aug/2025:00:37:02",
-                "method": "GET",
-                "path": "/dashboard",
-                "protocol": "HTTP/1.1",
-                "status": 404,
-                "size": 7317,
-                "referer": "-",
-                "user_agent": "curl/7.68.0"
-            },
-            {
-                "ip": "117.74.190.180",
-                "timestamp": "29/Aug/2025:00:37:02",
-                "method": "DELETE",
-                "path": "/products",
-                "protocol": "HTTP/1.1",
-                "status": 500,
-                "size": 2803,
-                "referer": "-",
-                "user_agent": "Wget/1.20.3 (linux-gnu)"
-            },
-            {
-                "ip": "207.238.22.214",
-                "timestamp": "29/Aug/2025:00:37:02",
-                "method": "POST",
-                "path": "/static/js/app.js",
-                "protocol": "HTTP/1.1",
-                "status": 404,
-                "size": 4086,
-                "referer": "-",
-                "user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) Safari/605.1.15"
-            },
-            {
-                "ip": "117.74.190.180",
-                "timestamp": "29/Aug/2025:00:37:02",
-                "method": "POST",
-                "path": "/dashboard",
-                "protocol": "HTTP/1.1",
-                "status": 503,
-                "size": 2195,
-                "referer": "-",
-                "user_agent": "Wget/1.20.3 (linux-gnu)"
-            },
-            {
-                "ip": "127.167.171.1",
-                "timestamp": "29/Aug/2025:00:37:02",
-                "method": "DELETE",
-                "path": "/dashboard",
-                "protocol": "HTTP/1.1",
-                "status": 502,
-                "size": 2418,
-                "referer": "-",
-                "user_agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64) Firefox/113.0"
-        }]
-        result = IP_frequencies(data_list)
-        expected = [{"ip": "127.167.171.1", "requests": 2}, {"ip": "117.74.190.180", "requests": 2}, {"ip": "207.238.22.214", "requests": 1}]
-        self.assertEqual(result, expected)
-'''
+        result = IP_frequencies([{},{}])
+        self.asserEqual(result, "")
+
     def test_status_code_destribution(self):
         result = status_code_destribution([{},{}])
         self.assertEqual(result, "")
@@ -389,4 +277,4 @@ class UnitTests(unittest.TestCase):
 '''
 
 if __name__ == "__main__":
-    unittest.main(verbosity=2)
+    unittest.main()
