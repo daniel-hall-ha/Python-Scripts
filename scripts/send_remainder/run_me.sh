820f54b1d82450761c0978655aca0aacfe12f6a5
daniel hall <ha.danielhall@gmail.com>
2025-09-29 16:45:49 +0630

    Revert "Revert "Finalize run_me.sh""
    This reverts commit 5536d0d7e8f7537d63f04690e3b39be7e4304f88.

--- scripts/send_remainder/run_me.sh
+++ scripts/send_remainder/run_me.sh
@@ -4,6 +4,8 @@ meeting_info=$(zenity --forms \
     --title 'Meeting' --text 'Remainder Information' \
     --add-calendar 'Date' --add-entry 'Title' \
     --add-entry 'Emails' \
+    --add-entry 'Meeting Link' \
+    --forms-date-format='%Y-%m-%d' \
     2>/dev/null)
 
 if [[ -n "$meeting_info" ]]; then
